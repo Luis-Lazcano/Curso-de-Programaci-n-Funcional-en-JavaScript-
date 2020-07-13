69eed30e0a6614aa9993b2dde1954ebdd0a208e4
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-12 21:49:04 -0500

    Clase 6 -Proyecto: Obtener el valor de los inputs

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+const description = document.getElementById('description');
+const calories = document.getElementById('calories');
+const carbs = document.getElementById('carbs');
+const proteins = document.getElementById('proteins');
