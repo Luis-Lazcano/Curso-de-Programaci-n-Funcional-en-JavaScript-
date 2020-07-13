c1f627ec9c6e2613b18b35ea41f8b4bd6bb358a5
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-13 11:22:19 -0500

    Clase 10 -Proyecto: Validar inputs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
+const IS_INVALID = 'is-invalid';
+
 const description = document.getElementById('description');
 const calories = document.getElementById('calories');
 const carbs = document.getElementById('carbs');
 const proteins = document.getElementById('proteins');
+
+const validateInputs = () => {
+  description.value ? '' : description.classList.add(IS_INVALID);
+  calories.value ? '' : calories.classList.add(IS_INVALID);
+  carbs.value ? '' : carbs.classList.add(IS_INVALID);
+  proteins.value ? '' : proteins.classList.add(IS_INVALID);
+
+  if(description.value && calories.value && carbs.value && proteins.value){
+    console.log('OK')
+  }
+
+}
+
+description.addEventListener('keydown', () => {
+  description.classList.remove(IS_INVALID);
+});
+calories.addEventListener('keydown', () => {
+  calories.classList.remove(IS_INVALID);
+});
+carbs.addEventListener('keydown', () => {
+  carbs.classList.remove(IS_INVALID);
+});
+proteins.addEventListener('keydown', () => {
+  proteins.classList.remove(IS_INVALID);
+});
