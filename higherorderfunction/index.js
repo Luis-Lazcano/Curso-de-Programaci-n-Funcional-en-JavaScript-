862da6ca9178a95ff8abc43e8ce4148109f20f4f
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-13 19:33:36 -0500

    Clase 18 -Introducción a las Higher Order Functions

---
+++ higherorderfunction/index.js
@@ -0,0 +1,15 @@
+// First Class Function
+const array1 = [1,2,3];
+const array2 = [];
+
+for (let i =0 ; i < array1.length; i++ ) {
+  array2.push(array1[i]*2);
+}
+
+console.log(array2) //[2,4,6]
+
+
+// Higher Orden Function
+const array1 = [1,2,3];
+const array2 = array1.map(item => item*2)
+console.log(array2);
